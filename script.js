7553a035406451d21b6e8b0ae965086f4393d13a
maria <omaria.ed@icloud.com>
2025-08-27 20:08:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//comunicaçao com banco de ddados - NOSQL - JSON
+
+fetch('dados.json').then(resposta => resposta.json()).then(banco => {
+    console.log(banco)
+
+    //font-end
+    document.getElementById('imagem').innerHTML =  banco[3].imagemProdutos.image001
+    document.getElementById('produto') .innerHTML = banco[0].produtos.prod123
+    document.getElementById('preco') .innerHTML = "R$" + banco[2].preco.preco791
+    document.getElementById('vendedor') .innerHTML = banco[1].vendedor.vend456
+
+     document.getElementById('imagem2').innerHTML =  banco[3].imagemProdutos.image002
+    document.getElementById('produto2') .innerHTML = banco[0].produtos.prod124
+    document.getElementById('preco2') .innerHTML = "R$" + banco[2].preco.preco790
+    document.getElementById('vendedor2') .innerHTML = banco[1].vendedor.vend457
+
+     document.getElementById('imagem3').innerHTML =  banco[3].imagemProdutos.image003
+    document.getElementById('produto3') .innerHTML = banco[0].produtos.prod125
+    document.getElementById('preco3') .innerHTML = "R$" + banco[2].preco.preco791
+    document.getElementById('vendedor3') .innerHTML = banco[1].vendedor.vend458
+
+    document.getElementById('imagem4').innerHTML =  banco[3].imagemProdutos.image004
+    document.getElementById('produto4') .innerHTML = banco[0].produtos.prod126
+    document.getElementById('preco4') .innerHTML = "R$" + banco[2].preco.preco792
+    document.getElementById('vendedor4') .innerHTML = banco[1].vendedor.vend459
+    document.getElementById('cliente4') .innerHTML = banco[4].cliente.cli501
+
+    document.getElementById('imagem5').innerHTML =  banco[3].imagemProdutos.image005
+    document.getElementById('produto5') .innerHTML = banco[0].produtos.prod127
+    document.getElementById('preco5') .innerHTML = "R$" + banco[2].preco.preco793
+    document.getElementById('vendedor5') .innerHTML = banco[1].vendedor.vend460
+    document.getElementById('cliente5') .innerHTML = banco[4].cliente.cli502
+})
